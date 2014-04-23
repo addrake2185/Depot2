48f9561f2b0a9f57639f7e5ca81d3b2e14098b16
Adam Drake <adam.drake1@gmail.com>
2014-04-22 20:07:27 -0500

    End of 14.3, 2 failures and 7 errors to fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_filter :authorize
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -11,5 +13,11 @@ class ApplicationController < ActionController::Base
   	cart = Cart.create
   	session[:cart_id] = cart.id
   	cart
+  end
+
+  def authorize
+    unless User.find_by_id(session[:user_id])
+      redirect_to login_url, notice: "Please log in"
+    end
   end
 end
